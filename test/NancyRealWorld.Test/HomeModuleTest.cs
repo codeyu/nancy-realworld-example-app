b84ecdc9fdec28f22dd37686c2d3feecc1890554
codeyu <codeyu@gmail.com>
2017-06-20 00:24:28 +0800

    add test project

---
+++ test/NancyRealWorld.Test/HomeModuleTest.cs
@@ -0,0 +1,25 @@
+﻿using Nancy;
+using Xunit;
+using Nancy.Testing;
+namespace NancyRealWorld.Test
+{
+    public class HomeModuleTest
+    {
+        [Fact]
+        public void TestHomeRoot()
+        {
+            //Arrange
+            var bootstrapper = new ConfigurableBootstrapper(with =>
+            {
+                with.Module<HomeModule>();
+            });
+            var browser = new Browser(bootstrapper);
+
+            //act
+            var response = browser.Get("/");
+
+            //assert
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode);
+        }
+    }
+}
